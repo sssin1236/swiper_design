3978b8d5cb8cc9bb29c6a1ce15b74badf4293b14
sssin1236 <suj0358@gmail.com>
2021-11-24 23:27:09 +0900

    첫번째 박스

--- js/main.js
+++ js/main.js
@@ -23,3 +23,30 @@ const swiper = new Swiper("#swiper", {
 });
 
 
+const bgs = document.querySelectorAll(".bg li");
+const prev = document.querySelector(".swiper-button-prev");
+const next = document.querySelector(".swiper-button-next");
+const navi = document.querySelectorAll(".swiper-pagination span");
+
+next.addEventListener("click", activation);
+prev.addEventListener("click", activation);
+window.addEventListener("mousewheel", activation);
+swiper.on("slideChangeTransitionEnd", activation);
+
+for(let el of navi){
+    el.addEventListener("click", e=>{
+        const isOn = e.currentTarget.classList.contains("swiper-pagination-bullet-active");
+        if(isOn) return;
+        swiper.on("slideChangeTransitionEnd", activation);
+    })
+}
+
+function activation(){
+    let item = document.querySelector(".swiper-slide-active");
+    let i = item.getAttribute("data-swiper-slide-index");
+
+    for(let el of bgs){
+        el.classList.remove("on");
+    }
+    bgs[i].classList.add("on");
+}
